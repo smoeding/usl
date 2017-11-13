3a8bd8a3712867fb546ea1a815bfdd70ffcccbff
Stefan Möding <stm@kill-9.net>
2017-11-13 21:27:54 +0100

    Test usl() with method=nls

---
+++ tests/t12.R
@@ -0,0 +1,14 @@
+#
+# Test method nls
+#
+
+library(usl)
+
+options(digits=3)
+
+dfr <- data.frame(load=c(1, 2,      4,      6,      8,      10),
+                  tput=c(1, 1.8868, 3.0769, 3.5294, 3.5398, 3.3557))
+
+try(u <- usl(tput ~ load, data=dfr, method="nls"))
+
+coef(u)
