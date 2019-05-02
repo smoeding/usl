6373aab933514d95bd4eaab936c0bf16a7899cfe
Stefan Möding <stm@kill-9.net>
2019-05-02 22:32:11 +0200

    Improve test coverage

--- tests/peak-scalability.R
+++ tests/peak-scalability.R
@@ -8,3 +8,6 @@ u <- usl(throughput ~ load, specsdm91)
 
 # Calculate where peak scalability is reached
 signif(peak.scalability(u), 2)
+
+# Calculate scalability for different coefficients
+signif(peak.scalability(u, 0.001, 0.00001), 2)
