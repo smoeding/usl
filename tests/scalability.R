d0f21412ace0aee095d6211c1b386d68ec552b9f
Stefan Möding <stm@kill-9.net>
2019-05-03 09:41:02 +0200

    Add test for scalability function

---
+++ tests/scalability.R
@@ -0,0 +1,15 @@
+# scalability.R --- Test function scalability
+
+library(usl)
+
+data(specsdm91)
+
+u <- usl(throughput ~ load, specsdm91)
+s1 <- scalability(u)
+s2 <- scalability(u, 0.001, 0.00001)
+
+# Calculate scalability
+signif(s1(20), 2)
+
+# Calculate scalability for different coefficients
+signif(s2(20), 2)
