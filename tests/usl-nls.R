540c3246d2fa8e9aa93fb20829b1817d4b5897be
Stefan Möding <stm@kill-9.net>
2019-12-30 17:40:26 +0100

    Update test results to take small rounding errors into account after changing solution method

--- tests/usl-nls.R
+++ tests/usl-nls.R
@@ -2,7 +2,7 @@
 
 library(usl)
 
-options(digits=3)
+options(digits=3, scipen=9)
 
 dfr <- data.frame(load=c(1, 2,      4,      6,      8,      10),
                   tput=c(1, 1.8868, 3.0769, 3.5294, 3.5398, 3.3557))
--- tests/usl-nlxb.R
+++ tests/usl-nlxb.R
@@ -2,6 +2,8 @@
 
 library(usl)
 
+options(digits=3, scipen=9)
+
 data(specsdm91)
 
 usl(throughput ~ load, specsdm91, method = "nlxb")
